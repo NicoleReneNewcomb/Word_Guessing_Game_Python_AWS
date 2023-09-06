7aaf05fe116bb9c37de395f3bc589ee77d5f0058
Nicole-Rene Newcomb <NicoleReneNewcomb@gmail.com>
2023-09-05 23:48:11 -0700

    Updated Comment Format
    Updated formatting of comments to increase readability.

--- python_game_NRN.py
+++ python_game_NRN.py
@@ -1,24 +1,30 @@
 #! /usr/bin/env python3.6
-#Word guessing game created by Nicole-Rene
+# Author: Nicole-Rene Newcomb
+# Description: Word guessing game that pulls
+#   words from txt file then creates a
+#   dictionary with word-length as key so
+#   user can select word length/difficulty
+#   and have a random word selected. Uses
+#   ASCII art to display Winner/You Lost.
 
 import os.path
 import random
 import string
 
-#Open external txt file containing dictionary words
+# Open external txt file containing dictionary words
 openFile = open(os.path.join(os.path.dirname(__file__), "8_nounlist.txt"))
 
-#Read file word entries as strings
+# Read file word entries as strings
 dictionary = openFile.read()
 
-#Declare dictionary to contain words
+# Declare dictionary to contain words
 dictionaryByLength = dict()
 
-#Group words into dictionary using word lengths as keys
+# Group words into dictionary using word lengths as keys
 for word in dictionary.split('\n'):
     dictionaryByLength.setdefault(len(word), []).append(word)
 
-#Output game header with instructions  
+# Output game header with instructions  
 print()
 print()
 print("========================================================")
@@ -27,22 +33,22 @@ print("========================================================")
 print()
 print("You may enter a difficulty/word length entry between 3-14")
 
-#Request user input for difficulty/word length
-#Checks if value is integer
+# Request user input for difficulty/word length
+# Checks if value is integer
 try:
     userLevel = int(input("\nHow many letters would you like in the word? "))
 except:
     print("\nYou must enter a number.")
     userLevel = 0
 
-#Check if entry is numeric and integer in range
+# Check if entry is numeric and integer in range
 while userLevel not in range(3,15):
     try:
         userLevel = int(input("\nPlease enter a number between 3 and 14: "))
     except:
         pass
 
-#Assign number of incorrect guesses allowed before losing
+# Assign number of incorrect guesses allowed before losing
 if userLevel in [3, 4]:
         wrongGuesses = 16
 elif userLevel in [5, 6, 7]:
@@ -52,50 +58,50 @@ elif userLevel in [8, 9, 10]:
 elif userLevel in [11, 12, 13, 14]:
         wrongGuesses = 8
 
-#Output user's entry and number of incorrect guesses allowed
+# Output user's entry and number of incorrect guesses allowed
 print("\nYour word will have", userLevel, "letters.")
 print("\nYou will lose if you make", wrongGuesses, "wrong guesses.")
 
-#Select secret word based on user's desired difficulty/length
+# Select secret word based on user's desired difficulty/length
 secretWord = random.choice(dictionaryByLength[int(userLevel)])
 
-#Create guessable word template
+# Create guessable word template
 wordImage = "_" * userLevel
 
-#Create list to hold previous guesses
+# Create list to hold previous guesses
 previousGuesses = '-'
 
-#Create player guess loop
+# Create player guess loop
 while wrongGuesses > 0:
   if "_" not in wordImage:
       break
   print("\nYou have", wrongGuesses, "wrong guesses left.")
   print("\nHere is the word so far:", wordImage)
   
-  #Input validation to ensure user enters single letter
+  # Input validation to ensure user enters single letter
   userCharacter = '-'
   while userCharacter not in string.ascii_lowercase:
     userCharacter = input("\nEnter a letter: ").lower()
     
-    #If multiple characters entered, use first one
+    # If multiple characters entered, use first one
     if len(userCharacter) > 1:
         userCharacter = userCharacter[0]
     
-    #If item entered not a letter, inform user
+    # If item entered not a letter, inform user
     if userCharacter not in string.ascii_lowercase:
         print("\nThat didn't seem to be a letter.")
 
-  #If user re-enters previously guessed character  
+  # If user re-enters previously guessed character  
   if userCharacter in wordImage or userCharacter in previousGuesses:
       print("\nYou've already guessed that letter.")
 
-  #If user character is in secret word
+  # If user character is in secret word
   elif userCharacter in secretWord:
       print("\nCongrats, that letter is in the word!")
       previousGuesses += userCharacter
       
-      #Fill in wordImage with guessed character
-      #Find multiple instances of characters in word
+      # Fill in wordImage with guessed character
+      # Find multiple instances of characters in word
       charInWord = True
 
       while charInWord == True:
@@ -106,7 +112,7 @@ while wrongGuesses > 0:
         secretWord = secretWord[:charIndex] + '1' + secretWord[charIndex+1:]
         wordImage = wordImage[:charIndex] + userCharacter + wordImage[charIndex+1:]
 
-  #If user character not in secret word
+  # If user character not in secret word
   elif userCharacter not in secretWord:
       print("\nSorry, that letter isn't in the word.")
       previousGuesses += userCharacter
@@ -115,7 +121,7 @@ while wrongGuesses > 0:
   else:
       print("\nOops. Something went wrong.")
 
-#If out of guesses, you lose
+# If out of guesses, you lose
 if wrongGuesses == 0:
     print("\nSorry, you lost this time. Please try again.")
     print(r"""
@@ -128,7 +134,7 @@ __   __                     _                  _
                                                            
 """)
 
-#If wordImage filled in, you win
+# If wordImage filled in, you win
 elif '_' not in wordImage:
     print("\nCongratulations! You've won!\n")
     print("Your winning word was:", wordImage, "\n")
